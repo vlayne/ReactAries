6affaa8af185ae16171b00f4b0b93c2123722bd6
Jimmy <jeremy.simonklein@gmail.com>
2018-12-17 17:02:07 +0100

    Added props call on signin form submit

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,11 +3,15 @@ import { SigninForm } from '../components'
 
 export default class Landing extends Component {
 
+    handleConnection = (data) => {
+        console.log("LANDING: SIGNIN FORM SUBMIT", data)
+    }
+
     render(){
         return(
             <Fragment>
                 <h1>LANDING COMPONENT</h1>
-                <SigninForm />
+                <SigninForm onFormSubmit={this.handleConnection} password_mode />
             </Fragment>
         )
     }
