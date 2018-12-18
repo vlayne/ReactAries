531f5362200ea60d6f2a8678038729f564d1c6e0
Jimmy <jeremy.simonklein@gmail.com>
2018-12-18 10:12:49 +0100

    Added handling of submit form on landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,15 +3,28 @@ import { SigninForm } from '../components'
 
 export default class Landing extends Component {
 
+    state = {
+
+    }
+
     handleConnection = (data) => {
-        console.log("LANDING: SIGNIN FORM SUBMIT", data)
+        this.setState({
+            logged: true,
+            username: data.username,
+            email: data.email
+        })
     }
 
     render(){
+        const { username, email, logged } = this.state
         return(
             <Fragment>
                 <h1>LANDING COMPONENT</h1>
-                <SigninForm onFormSubmit={this.handleConnection} password_mode />
+                {logged ? 
+                    <p>Utilisateur connecté : {username}, son email est {email}</p>    
+                :
+                    <SigninForm onFormSubmit={this.handleConnection} />
+                }
             </Fragment>
         )
     }
